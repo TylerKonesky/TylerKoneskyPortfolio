56451db6dcb67754d79c58baa9a5240bd5797e5f
TylerKonesky <tylerkonesky@hotmail.com>
2020-08-22 19:00:38 -0600

    json update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,10 +3,6 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
 
-
-import Admin from './Admin/Admin';
-import Header from './Header/Header';
-import Landing from './Landing/Landing';
 import Portfolio from '../SPAPort/portfolio';
 
 class App extends Component{
@@ -18,12 +14,8 @@ class App extends Component{
       <div>
         <BrowserRouter>
           <div>
-            {/* <Header /> */}
             <Switch>
               <Route exact path="/" component={Portfolio}/>
-              {/* <Route exact path="/" component={Landing}/>
-              <Route path="/pageAdmin" component={Admin}/> */}
-
             </Switch>
           </div>
         </BrowserRouter>
